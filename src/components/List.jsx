48f77de602fe83b9d5fcc05e428fa4a9254a4ae4
Jayyoo2019 <Jayyoo2019@gmail.com>
2022-04-16 21:23:49 +0900

    tictactoe

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,7 @@
 import React, {useState} from 'react'
+import ReactDom from 'react-dom';
+import '../ttt.css'
+import Game from './tictactoe';
 
 export default function List() {
     const [list, setList] = useState(['Programming']);
@@ -14,6 +17,23 @@ export default function List() {
             console.log("else")
             setList([...list, newTask])
             setNewTask('')
+            
+            const d = new Date('December 17, 1995 03:24:00');
+            const s = d.toString();
+            console.log(d)
+            console.log(s)
+            console.log(d==s);
+
+            const string1 = "hello"
+            const string2 = String(string1)
+            const string3 = new String("hello")
+            const string4 = new String("hello")
+            console.log(string2)
+            console.log(string2 == string3)
+            console.log(string3 == string4)
+            console.log(string4)
+            console.log("https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Equality")
+            
         }else{
             alert("should not be empty!")
         }
@@ -27,6 +47,7 @@ export default function List() {
 
     return (
         <div>
+            <Game/>
             <p>
                 to do list: {list.length}
             </p>
